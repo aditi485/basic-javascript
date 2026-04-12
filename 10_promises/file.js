b5ed1545b64973ddb8e6e71aff526caec0fc1fe7
aditi485 <tripathiaditi121@gmail.com>
2026-04-12 12:18:30 +0000

    new learning

--- 10_promises/file.js
+++ 10_promises/file.js
@@ -7,13 +7,12 @@
    setTimeout(function(){
     console.log('hey task async')
     resolve(); // return value to consume
-   },2000)
+   },1000)
  })
 
- // promise consume
+ //promise consume
  promise.then(function(){
-    console.log("promise consumed")
- })
+    console.log("promise consumed") })
 
 
 new Promise(function(resolve,reject){
@@ -28,13 +27,76 @@ const promiseOne = new Promise(function(resolve,reject){
     setTimeout(function(){
         resolve({username: 'adititripathi',study:'btech',   // resolve returns the value in then function
             college:'ghaziabad'
-        },3000)
+        },1000)
     })
 
 })
- promiseOne.then(function(user){
+ promiseOne.then(function(user){// resolve and then are connected so by default there will data. come in this
     console.log(user)
 
  })
 
+
+ const promisefour = new Promise(function(reject,resolve){
+    setTimeout(function(){
+       let error = false
+       if(!error){
+          resolve({ 
+            username: "aditi tripathi",
+            course: "btech",
+            semester: 4})
+       }
+       else{
+           reject("ERROR Something went wrong")
+       }
+    },1000)
+ })
+
+ promisefour.then((user)=>{
+    console.log(user);
+    return user.username
+  }).then((username) =>{
+    console.log(username)
+ })
+ .catch( function(error){ // reject and catch are directly connected with each other
+    console.log(error)
+ }).finally(()=>{
+    console.log("the promise finally executed ")
+ })
+
+const promisefive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = false;
+       if(!error){
+        resolve({id:"abc123", company:"infosys"})
+       }
+       else{
+        reject("NEW ERORR!!!")
+       }
+    },1000)
+}) 
+async function consumePromise() {
+    try{
+        const response = await promisefive;
+        console.log(response)
+    }catch(error){
+        console.log(error)
+    }
+}
+consumePromise()
+
+async function fetching(){
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+        console.log(data);
+    }catch(error){
+        console.log(erorr)
+    }
+}
+fetching()
+fetch('https://jsonplaceholder.typicode.com/users').then((response)=>{
+    return response.json()
+}).then((data)=> console.log(data))
+.catch((error)=> console.log(error))
  
